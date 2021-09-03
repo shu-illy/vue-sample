2436359c6c07d0bb8d8f5624b9fc60a236b48bf9
Shuhei Iriyama <67177058+shu-illy@users.noreply.github.com>
2021-09-04 02:51:15 +0900

    Chapter 84

--- src/store/index.js
+++ src/store/index.js
@@ -24,6 +24,16 @@ export default new Vuex.Store({
       address.id = id
       state.addresses.push(address);
     },
+    updateAddress(state, { id, address }) {
+      const index = state.addresses.findIndex(address => address.id === id)
+
+      state.addresses[index] = address
+    },
+    deleteAddress(state, { id }) {
+      const index = state.addresses.findIndex(address => address.id === id)
+
+      state.addresses.splice(index, 1)
+    }
   },
   actions: {
     setLoginUser({ commit }, user) {
@@ -54,10 +64,25 @@ export default new Vuex.Store({
         })
       }
     },
+    updateAddress({ getters, commit }, { id, address }) {
+      if (getters.uid) {
+        firebase.firestore().collection(`users/${getters.uid}/addresses`).doc(id).update(address).then(() => {
+          commit("updateAddress", { id, address })
+        })
+      }
+    },
+    deleteAddress({ getters, commit }, { id }) {
+      if (getters.uid) {
+        firebase.firestore().collection(`users/${getters.uid}/addresses`).doc(id).delete().then(() => {
+          commit("deleteAddress", { id })
+        })
+      }
+    }
   },
   getters: {
     userName: state => state.login_user ? state.login_user.displayName : '',
     photoURL: state => state.login_user ? state.login_user.photoURL : '',
-    uid: state => state.login_user ? state.login_user.uid : null
+    uid: state => state.login_user ? state.login_user.uid : null,
+    getAddressById: state => id => state.addresses.find(address => address.id === id)
   }
 });
--- src/views/Addresses.vue
+++ src/views/Addresses.vue
@@ -20,6 +20,7 @@
             <router-link :to="{ name: 'address_edit', params: { address_id: item.id }}">
               <v-icon small class="mr-2">mdi-pencil</v-icon>
             </router-link>
+            <v-icon small class="mr-2" @click="deleteConfirm(item.id)">mdi-delete</v-icon>
           </template>
         </v-data-table>
       </v-flex>
@@ -28,6 +29,7 @@
 </template>
 
 <script>
+  import { mapActions } from 'vuex'
   export default {
     created() {
       this.addresses = this.$store.state.addresses
@@ -43,6 +45,14 @@
         ],
         addresses: []
       }
+    },
+    methods: {
+      deleteConfirm (id) {
+        if (confirm('削除してよろしいですか？')) {
+          this.deleteAddress({ id })
+        }
+      },
+      ...mapActions(['deleteAddress'])
     }
   }
 </script>
